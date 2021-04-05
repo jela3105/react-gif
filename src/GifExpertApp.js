1ce4337b9fa426fa82b8de1f2c9616738a122da1
jela3105 <jela3105@gmail.com>
2021-04-05 15:29:26 -0500

    Create a categories list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+  //const categories = ["Imagine Dragons", "Twenty One Pilots", "React"];
+  const [categories, setCategories] = useState([
+    "Imagine Dragons",
+    "Twenty One Pilots",
+    "React",
+  ]);
+  const handleAdd = () => {
+    //setCategories([...categories, "NF"]);
+    setCategories((oldCategories) => [...oldCategories, "NF"]);
+  };
   return (
     <div>
       <h2>GifExpertApp</h2>
       <hr />
+      <button onClick={handleAdd}>Add</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {categories}
     </div>
   );
 };
