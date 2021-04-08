27884dec801b2661e674805846b50487c2186da9
jela3105 <jela3105@gmail.com>
2021-04-07 22:47:22 -0500

    Get images from API

---
+++ src/components/GifGrid.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=twenty one pilots&limit=10&api_key=" +
+      process.env.REACT_APP_API_KEY;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        img: img.images?.downsized_medium.url,
+      };
+    });
+    console.log(gifs);
+  };
+  getGifs();
+  return (
+    <div>
+      <h3>{category}</h3>
+    </div>
+  );
+};
