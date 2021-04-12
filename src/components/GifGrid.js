528b71a0665f0827d62c59d6dc80f41d349445cc
jela3105 <jela3105@gmail.com>
2021-04-11 22:59:08 -0500

    Create helper to get Gifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,27 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { GifGridItem } from "./GifGridItem";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
   useEffect(() => {
-    getGifs();
-  }, []);
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
-      category
-    )}&limit=10&api_key=${process.env.REACT_APP_API_KEY}`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    setImages(gifs);
-  };
-  //  getGifs();
+    getGifs(category).then(setImages);
+  }, [category]);
+
   return (
     <>
       <h3>{category}</h3>
